87e4c558a57deaafe8b4c2ca2c22c688e56ee098
exdisj <exdisj@outlook.com>
2025-06-07 22:04:53 -0400

    Worked on graphical programming, specifically styles and including features for different graphics pipelines

--- src/ui/windows/graphics.rs
+++ src/ui/windows/graphics.rs
@@ -1,6 +1,17 @@
 use std::fmt::Display;
 
-use windows::Win32::Foundation::HWND;
+use windows::Win32::{Foundation::HWND};
+#[cfg(feature = "gdi_graphics")]
+use windows::Win32::Foundation::COLORREF;
+
+#[cfg(feature = "gdi_plus_graphics")]
+use windows::Win32::Graphics::GdiPlus::Color as GdiColor;
+
+#[cfg(feature = "direct_graphics")]
+use windows::Win32::Graphics::Direct2D::Common::D2D1_COLOR_F;
+
+#[cfg(feature = "direct3d_graphics")]
+use windows::Win32::Graphics::Direct3D9::D3DCOLORVALUE;
 
 use crate::ui::tool::utf_str::Utf16String;
 
@@ -11,6 +22,115 @@ pub struct ResourceKey {
 impl Display for ResourceKey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         todo!()
+    }
+}
+
+pub struct ColorResource {
+    alpha: u8,
+    red: u8,
+    green: u8,
+    blue: u8
+}
+#[cfg(feature = "gdi_graphics")]
+impl Into<COLORREF> for ColorResource {
+    fn into(self) -> COLORREF {
+        let val = self.red as u32 | (self.green as u32) << 8 | (self.blue as u32) << 16;
+        COLORREF(val)
+    }
+}
+#[cfg(feature = "gdi_plus_graphics")]
+impl Into<GdiColor> for ColorResource {
+    fn into(self) -> GdiColor {
+        let value = ((self.alpha as u32) << 24)
+              | ((self.red as u32) << 16)
+              | ((self.green as u32) << 8)
+              | (self.blue as u32);
+
+       GdiColor { Argb: value }
+    }
+}
+#[cfg(feature = "direct_graphics")]
+impl Into<D2D1_COLOR_F> for ColorResource {
+    fn into(self) -> D2D1_COLOR_F {
+        let max = u8::MAX as f32;
+        D2D1_COLOR_F { 
+            g: self.green as f32 / max,
+            r: self.red   as f32 / max, 
+            b: self.blue  as f32 / max, 
+            a: self.alpha as f32 / max 
+        }
+    }
+}
+#[cfg(feature = "direct3d_graphics")]
+impl Into<D3DCOLORVALUE> for ColorResource {
+    fn into(self) -> D3DCOLORVALUE {
+        let max = u8::MAX as f32;
+        D3DCOLORVALUE { 
+            r: self.red as f32   / max, 
+            g: self.green as f32 / max, 
+            b: self.blue as f32  / max, 
+            a: self.alpha as f32 / max 
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Backgrounds {
+    Primary,
+    Secondary, 
+    Tertiary
+}
+
+pub enum StyleAccess {
+    Background(Backgrounds),
+    Accent,
+    Foreground
+}
+
+pub trait StyleBook {
+    type Storage;
+
+    fn get(&self, rc: StyleAccess) -> &Self::Storage;
+    fn get_mut(&mut self, rc: StyleAccess) -> &mut Self::Storage;
+}
+
+pub struct StyleTemplate {
+    id: u32,
+    background: [ColorResource; 3],
+    accent: ColorResource,
+    foreground: ColorResource
+}
+impl StyleBook for StyleTemplate {
+    type Storage = ColorResource;
+
+    fn get(&self, rc: StyleAccess) -> &ColorResource {
+        match rc {
+            StyleAccess::Accent => &self.accent,
+            StyleAccess::Foreground => &self.foreground,
+            StyleAccess::Background(b) => &self.background[b as u8 as usize]
+        }
+    }
+    fn get_mut(&mut self, rc: StyleAccess) -> &mut ColorResource {
+        match rc {
+            StyleAccess::Accent => &mut self.accent,
+            StyleAccess::Foreground => &mut self.foreground,
+            StyleAccess::Background(b) => &mut self.background[b as u8 as usize]
+        }
+    }
+}
+impl StyleTemplate {
+    pub fn new(id: u32, background: [ColorResource; 3], accent: ColorResource, foreground: ColorResource) -> Self {
+        Self {
+            id,
+            background,
+            accent,
+            foreground
+        }
+    }
+
+    pub fn id(&self) -> u32 {
+        self.id
     }
 }
 
